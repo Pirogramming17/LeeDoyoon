2f2c0dfbed0fe2fa3dd3eaedeb89e4a08aca2c36
Doyun Lee <doyn511@ajou.ac.kr>
2022-07-08 17:20:36 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,14 @@
-num = 0
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+num = 0 #입력 받는 숫자(1,2,3 중 하나)
+cnt = 1 #1부터 31까지 카운트 누적
+
+while(True):
+  try:
+    num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+  except ValueError:
+    print("정수를 입력하세요")
+  else : 
+    if(num != 1 and num != 2 and num != 3):
+      print("1, 2, 3 중 하나만 입력하세요")
+    else:
+      break;
+  
