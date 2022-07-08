30c739cd498bb725f710b1547fbaa0c126cf014e
Doyun Lee <doyn511@ajou.ac.kr>
2022-07-08 17:24:38 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -16,3 +16,19 @@ while(True):
 for i in range(0, num):
   print("playerA : " , cnt)
   cnt += 1
+
+# player B
+while(True):
+  try:
+    num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+  except ValueError:
+    print("정수를 입력하세요")
+  else : 
+    if(num != 1 and num != 2 and num != 3):
+      print("1, 2, 3 중 하나만 입력하세요")
+    else:
+      break;
+
+for i in range(0, num):
+  print("playerB : ", cnt)
+  cnt += 1
