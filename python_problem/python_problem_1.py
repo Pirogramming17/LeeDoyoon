05dd598feb13f18c196e29744d90b417e1ee24b8
Doyun Lee <doyn511@ajou.ac.kr>
2022-07-08 18:11:10 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,42 @@
 num = 0 #입력 받는 숫자(1,2,3 중 하나)
 cnt = 1 #1부터 31까지 카운트 누적
 
-# player A
-while(True):
-  try:
-    num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-  except ValueError:
-    print("정수를 입력하세요")
-  else : 
-    if(num != 1 and num != 2 and num != 3):
-      print("1, 2, 3 중 하나만 입력하세요")
-    else:
+while(cnt <= 31):
+    # player A
+  while(True):
+    try:
+      num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+    except ValueError:
+      print("정수를 입력하세요")
+    else : 
+      if(num != 1 and num != 2 and num != 3):
+        print("1, 2, 3 중 하나만 입력하세요")
+      else:
+        break;  
+  for i in range(0, num):
+    if cnt > 31:
       break;
-  
-for i in range(0, num):
-  print("playerA : " , cnt)
-  cnt += 1
-
-# player B
-while(True):
-  try:
-    num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-  except ValueError:
-    print("정수를 입력하세요")
-  else : 
-    if(num != 1 and num != 2 and num != 3):
-      print("1, 2, 3 중 하나만 입력하세요")
-    else:
+    else: 
+      print("playerA : " , cnt)
+      cnt += 1
+  if(cnt > 31):
+    break;
+    # player B
+  while(True):
+    try:
+      num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+    except ValueError:
+      print("정수를 입력하세요")
+    else : 
+      if(num != 1 and num != 2 and num != 3):
+        print("1, 2, 3 중 하나만 입력하세요")
+      else:
+        break;
+  for i in range(0, num):
+    if cnt > 31:
       break;
-
-for i in range(0, num):
-  print("playerB : ", cnt)
-  cnt += 1
+    else: 
+      print("playerB : ", cnt)
+      cnt += 1
+  if(cnt > 31):
+    break;
